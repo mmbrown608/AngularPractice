e9c987607e563e0b16bf2dddab1bd5fd3998d21f
mmbrown608 <melissa.brown@veritix.com>
2014-09-08 08:54:03 -0400

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,16 @@
+ 'use strict';
+ 
+ /* Controllers */
++
++var ticketsApp = angular.module('ticketsApp', []);
++
+ticketsApp.controller('TeamListCtrl', function($scope) {
++  $scope.teams = [
++    {'name': 'Detroit Lions',
++     'snippet': 'NFL'},
++    {'name': 'Cleveland Cavaliers',
++     'snippet': 'NBA'},
++    {'name': 'Houston Rockets',
++     'snippet': 'NBA'}
++  ];
++});
